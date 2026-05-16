378a704ec1512ccb1c9ea8a627616d3a34b67280
FRAmyZ010 <pongsapat.pp2004@gmail.com>
2026-05-17 00:06:36 +0700

    Complete API Post Recipes

--- backend/src/routes/recipes.js
+++ backend/src/routes/recipes.js
@@ -14,4 +14,39 @@ router.get("/", (req,res)=>{
     })
 })
 
+router.post("/",(req,res)=>{
+     const {
+        name,
+        ingredients,
+        instructions,
+        image_url,
+        category_id,
+        cook_time,
+        servings
+     } = req.body;
+
+     const insertRecipe = `
+        INSERT INTO recipes (
+        name,
+        ingredients,
+        instructions,
+        image_url,
+        category_id,
+        cook_time,
+        servings
+        )
+        VALUES (?,?,?,?,?,?,?)
+     `;
+
+     db.run(insertRecipe, [name, ingredients, instructions, image_url, category_id, cook_time, servings],(err)=>{
+        if(err){
+            res.status(500).json({error: err.message});
+            console.log("Error inserting recipe:", err.message);
+        }
+        else{
+            res.status(201).json({message: "Recipe added successfully"});
+        }
+     })
+})
+
 module.exports = router;
