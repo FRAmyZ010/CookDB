fb40f6f2874d72af3b761150f28fa3064eb15cb4
FRAmyZ010 <pongsapat.pp2004@gmail.com>
2026-05-17 00:16:17 +0700

    Complete API delete recipes

--- backend/src/routes/recipes.js
+++ backend/src/routes/recipes.js
@@ -44,9 +44,37 @@ router.post("/",(req,res)=>{
             console.log("Error inserting recipe:", err.message);
         }
         else{
-            res.status(201).json({message: "Recipe added successfully"});
+            res.status(201).json({
+                message: "Recipe added successfully",
+                id: this.lastID
+            });
         }
      })
+})
+
+router.delete("/:id",(req,res)=>{
+    const id = req.params.id;
+
+    const deleteRecipe = `DELETE FROM recipes WHERE id = ?`;
+
+    db.run(deleteRecipe,[id],(err)=>{
+        if(err){
+            return res.status(500).json({
+                error: err.message,
+                message: "Error deleting recipe"
+            })
+        }
+        if (this.changes === 0){
+            return res.status(404).json({
+                message: "Recipe not found"
+            })
+        }
+
+        res.status(200).json({
+            message: "Recipe deleted successfully",
+            id:id
+        })
+    })
 })
 
 module.exports = router;
