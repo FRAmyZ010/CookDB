f037ec96ee95d96969252877c97566868b186e6c
FRAmyZ010 <pongsapat.pp2004@gmail.com>
2026-05-17 23:46:57 +0700

    Complete API show all categories

--- backend/src/routes/category.js
+++ backend/src/routes/category.js
@@ -3,6 +3,23 @@ const router = express.Router();
 
 const db = require("../database");
 
+// GET ALL CATEGORIES
+router.get("/",(req,res)=>{
+    const showCat = `
+    SELECT *
+    FROM categories
+    `;
+
+    db.all(showCat,(err,row)=>{
+        if(err){
+            return res.status(500).json({
+                err:err.message
+            })
+        }
+        res.status(200).json(row)
+    })
+})
+
 
 
 module.exports = router
