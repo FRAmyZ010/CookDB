6a30ef82c9cfd0964673221667bc6b6a201ec0e2
FRAmyZ010 <pongsapat.pp2004@gmail.com>
2026-05-18 09:32:05 +0700

    Complete API update category

--- backend/src/routes/category.js
+++ backend/src/routes/category.js
@@ -22,18 +22,40 @@ router.get("/",(req,res)=>{
 
 // CREATE NEW CATEGORY
 router.post("/",(req,res)=>{
-    const name = req.body.name
+    const name = req.body.name;
     const creatCat = `
     INSERT INTO categories (name) VALUES (?);
     `;
 
     db.run(creatCat,[name],(err)=>{
         if(err){
-            return res.status(500).json({err:err.message})
+            return res.status(500).json({error:err.message})
         }
         res.status(201).json({
             message:`Created category "${name}" successfully!`,
             id : this.lastID
+        })
+    })
+})
+
+// UPDATE CATEGORY
+router.put("/:id",(req,res)=>{
+    const id = req.params.id;
+    const name = req.body.name;
+
+    const updtCat = `UPDATE categories SET name = ? WHERE id = ?`;
+    
+    db.run(updtCat,[name, id],(err,result)=>{
+        if(err){
+            return res.status(500).json({
+                err:err.message
+            })
+        }
+        
+        res.status(201).json({
+            message:"Updated successfully!",
+            id:id,
+            name:name
         })
     })
 })
