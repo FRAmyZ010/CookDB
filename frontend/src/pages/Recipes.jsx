512dc05069978b98fa132890adb3cdbce4465f8d
FRAmyZ010 <pongsapat.pp2004@gmail.com>
2026-05-19 11:01:16 +0700

    Complete Navbar components

---
+++ frontend/src/pages/Recipes.jsx
@@ -0,0 +1,23 @@
+function Recipes() {
+  return (
+    <>
+      <h1>Recipes</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem
+        nihil ea consectetur sint earum illum numquam, ipsum ducimus natus ut
+        qui eaque est repudiandae quas magnam eligendi quos totam nam. Rerum
+        nihil atque sit quisquam, iure cupiditate facilis fugiat reprehenderit
+        quae hic, asperiores voluptas molestiae vel odit modi! Cumque voluptatum
+        illo molestias voluptas dignissimos, quis corporis voluptate dolore
+        voluptates vel quisquam ratione provident animi, non veniam facilis.
+        Provident, in. Libero doloribus adipisci ullam et temporibus
+        necessitatibus, reprehenderit veritatis a suscipit earum, quam dolorum
+        neque animi qui magnam aspernatur accusantium mollitia perspiciatis
+        aperiam beatae pariatur assumenda nemo delectus. Commodi, repellendus
+        earum?
+      </p>
+    </>
+  );
+}
+
+export default Recipes;
