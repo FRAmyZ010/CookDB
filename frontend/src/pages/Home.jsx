1f1c667ec47ba64d4c18a69e65df3c2e8282a58a
FRAmyZ010 <pongsapat.pp2004@gmail.com>
2026-05-21 10:49:57 +0700

    files: Adding image files to /public/img

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,15 @@
+import "./Home.css"
+
+import RecipesCard from "../components/RecipesCard"
+
 function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis
-        reiciendis, facere possimus repudiandae tenetur totam sed? Quisquam
-        velit fugiat ad illo nemo possimus, similique praesentium a sed eaque
-        doloremque deleniti, dignissimos delectus ut autem in inventore repellat
-        voluptatum explicabo fugit earum corporis. Quos corrupti doloribus unde
-        corporis adipisci quibusdam magnam, minima obcaecati praesentium.
-        Tenetur placeat unde vitae temporibus beatae, officiis minima.
-        Repudiandae id debitis fuga eum voluptate ipsum labore laboriosam sunt
-        laborum doloribus tempore fugit, sed odio animi quas provident nulla
-        maiores rerum nam mollitia numquam maxime, minus nostrum vero?
-        Voluptatibus vel eum adipisci impedit blanditiis rem quasi mollitia
-        consequuntur.
-      </p>
+      <div className="container">
+        <RecipesCard/>
+      </div>
     </>
+    
   );
 }
 
