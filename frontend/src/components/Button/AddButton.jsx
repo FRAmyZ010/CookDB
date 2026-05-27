fb590caa90e1e9b567a96b264a33b69748612802
FRAmyZ010 <pongsapat.pp2004@gmail.com>
2026-05-27 11:14:46 +0700

    Complete API create new recipes!!!

--- frontend/src/components/Button/AddButton.jsx
+++ frontend/src/components/Button/AddButton.jsx
@@ -6,24 +6,46 @@ import { useEffect, useState } from "react";
 const AddButton = () => {
   const [add, setAdd] = useState(false);
 
-  const [name,setName] = useState("");
-  const [time,setTime] = useState("");
-  const [servings,setServings] = useState("");
-  const [ingredients,setIngredients] = useState("");
-  const [instructions,setInstructions] = useState("");
-  const [image,setImage] = useState(null)
+  const [name, setName] = useState("");
+  const [time, setTime] = useState("");
+  const [servings, setServings] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const [image, setImage] = useState(null);
 
-  function saveData(e){
-    e.preventDefault()
-    const formData = {
-      name:name,
-      time:time,
-      servings:servings,
-      ingredients:ingredients,
-      instructions:instructions,
-      image:image
+  async function saveData(e) {
+    e.preventDefault();
+
+    try {
+      // create FormData
+      const formData = new FormData();
+
+      // append
+      formData.append("name", name);
+      formData.append("cook_time", time);
+      formData.append("servings", servings);
+      formData.append("ingredients", ingredients);
+      formData.append("instructions", instructions);
+      formData.append("image", image);
+
+      // API POST
+
+      const response = await fetch("http://localhost:3000/recipes", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      console.log(data);
+      alert("Recipes Added!");
+
+      if (response.ok) {
+        window.location.reload();
+      }
+    } catch (err) {
+      console.log(err);
     }
-    console.log(formData)
   }
 
   return (
@@ -44,25 +66,81 @@ const AddButton = () => {
             </div>
             <div className="form-container">
               <form onSubmit={saveData}>
-                <label htmlFor="">Name</label>
-                <input value={name} type="text" name="name" id="" onChange={(e)=>setName(e.target.value)} required/>
+                <label htmlFor="">
+                  Name <span title="Enter your recipe's name.">ⓘ</span>
+                </label>
+                <input
+                  value={name}
+                  type="text"
+                  name="name"
+                  id=""
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
 
                 <label htmlFor="">Time</label>
-                <input value={time} type="text" name="time" id=""  onChange={(e)=>setTime(e.target.value)}/>
+                <input
+                  value={time}
+                  type="text"
+                  name="time"
+                  id=""
+                  onChange={(e) => setTime(e.target.value)}
+                  required
+                />
 
                 <label htmlFor="">Serving</label>
-                <input value={servings} type="text" name="serving" id="" onChange={(e)=>setServings(e.target.value)} />
+                <input
+                  value={servings}
+                  type="text"
+                  name="serving"
+                  id=""
+                  onChange={(e) => setServings(e.target.value)}
+                  required
+                />
 
                 <label htmlFor="">Ingredients</label>
-                <textarea value={ingredients} type="text" name="ingredients" id="" onChange={(e)=>setIngredients(e.target.value)}/>
+                <textarea
+                  value={ingredients}
+                  type="text"
+                  name="ingredients"
+                  id=""
+                  onChange={(e) => setIngredients(e.target.value)}
+                  required
+                />
 
                 <label htmlFor="">Instructions</label>
-                <textarea value={instructions} type="text" name="instructions" id="" onChange={(e)=>setInstructions(e.target.value)}/>
+                <textarea
+                  value={instructions}
+                  type="text"
+                  name="instructions"
+                  id=""
+                  onChange={(e) => setInstructions(e.target.value)}
+                  required
+                />
 
                 <label htmlFor="">Image</label>
-                <input type="file" name="" id="" accept="image/*" onChange={(e)=>setImage(e.target.files[0])}/>
+                <input
+                  type="file"
+                  name=""
+                  id=""
+                  accept="image/*"
+                  onChange={(e) => setImage(e.target.files[0])}
+                  required
+                />
 
-                <button type="submit">SAVE</button>
+                <button
+                  type="submit"
+                  disabled={
+                    name.trim() === "" ||
+                    time.trim() === "" ||
+                    servings.trim() === "" ||
+                    ingredients.trim() === "" ||
+                    instructions.trim() === "" ||
+                    image === null
+                  }
+                >
+                  SAVE
+                </button>
               </form>
             </div>
           </div>
