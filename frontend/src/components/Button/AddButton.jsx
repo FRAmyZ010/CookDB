055e287a7f5fb73fa3134f9de1e01c4aa8767c05
FRAmyZ010 <pongsapat.pp2004@gmail.com>
2026-05-27 09:20:07 +0700

    Complete getting form data

--- frontend/src/components/Button/AddButton.jsx
+++ frontend/src/components/Button/AddButton.jsx
@@ -6,6 +6,26 @@ import { useEffect, useState } from "react";
 const AddButton = () => {
   const [add, setAdd] = useState(false);
 
+  const [name,setName] = useState("");
+  const [time,setTime] = useState("");
+  const [servings,setServings] = useState("");
+  const [ingredients,setIngredients] = useState("");
+  const [instructions,setInstructions] = useState("");
+  const [image,setImage] = useState(null)
+
+  function saveData(e){
+    e.preventDefault()
+    const formData = {
+      name:name,
+      time:time,
+      servings:servings,
+      ingredients:ingredients,
+      instructions:instructions,
+      image:image
+    }
+    console.log(formData)
+  }
+
   return (
     <>
       <button className="addBtn" onClick={() => setAdd(true)}>
@@ -23,24 +43,24 @@ const AddButton = () => {
               </button>
             </div>
             <div className="form-container">
-              <form>
+              <form onSubmit={saveData}>
                 <label htmlFor="">Name</label>
-                <input type="text" name="name" id="" />
+                <input value={name} type="text" name="name" id="" onChange={(e)=>setName(e.target.value)} required/>
 
                 <label htmlFor="">Time</label>
-                <input type="text" name="time" id="" />
+                <input value={time} type="text" name="time" id=""  onChange={(e)=>setTime(e.target.value)}/>
 
                 <label htmlFor="">Serving</label>
-                <input type="text" name="serving" id="" />
+                <input value={servings} type="text" name="serving" id="" onChange={(e)=>setServings(e.target.value)} />
 
                 <label htmlFor="">Ingredients</label>
-                <input type="text" name="ingredients" id="" />
+                <textarea value={ingredients} type="text" name="ingredients" id="" onChange={(e)=>setIngredients(e.target.value)}/>
 
                 <label htmlFor="">Instructions</label>
-                <input type="text" name="instructions" id="" />
+                <textarea value={instructions} type="text" name="instructions" id="" onChange={(e)=>setInstructions(e.target.value)}/>
 
                 <label htmlFor="">Image</label>
-                <input type="file" name="" id="" accept="image/*"/>
+                <input type="file" name="" id="" accept="image/*" onChange={(e)=>setImage(e.target.files[0])}/>
 
                 <button type="submit">SAVE</button>
               </form>
